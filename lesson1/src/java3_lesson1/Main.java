0daffb1995b73e24706659a086c1a3c7978df2c0
Amina <amina1000@yandex.ru>
2020-02-24 23:28:32 +0300

    Task1

--- lesson1/src/java3_lesson1/Main.java
+++ lesson1/src/java3_lesson1/Main.java
@@ -1,8 +1,44 @@
 package java3_lesson1;
+import java.util.Arrays;
 
+/**
+ * java.level3.lesson1 example of homework
+ *
+ *
+ * @auphtor Isaeva Amina
+ * @version dated february 24,2020
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
+       Integer arr1[] = {1, 2, 3, 4, 5, 6, 7};
+       try
+       {
+       swapArray(arr1,2,4);
+    } catch (WrongPositionException e){
+       e.printStackTrace();}
     }
+    //2. Написать метод, который преобразует массив в ArrayList;
+    /**
+     *
+     * @param arr массив данных ссылочного типа
+     * @param position1 елемент массива, который нужно заменить
+     * @param position2 елемент массива, на который нужно заменить
+     */
+    public static void swapArray(Object[] arr, int position1, int position2) throws WrongPositionException{
+        System.out.println("Массив данных: "+ Arrays.toString(arr));
+        if (position1 < 0 || position1> arr.length ||
+                position2 < 0 || position2 > arr.length || position1 == position2) {
+            throw new WrongPositionException("Неправильно указаны позиции элементов для замены");
+        }
+
+        Object sw = arr[position1];
+        arr[position1]=arr[position2];
+        arr[position2]=sw;
+        System.out.println("Массив преобразованных данных: "+Arrays.toString(arr));
+    }
+
 }
+
+
